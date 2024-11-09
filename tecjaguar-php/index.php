969e8078a98b842f40acfba05116a8cc821f8ee6
João Pedro M <joaopmeloribeiro@gmail.com>
2024-11-09 19:18:42 -0300

    teste-php
    algumas alterações e adições usando php para teste

---
+++ tecjaguar-php/index.php
@@ -0,0 +1,60 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="shortcut icon" type="image/png" href="Jaguar.ico">
+    <link rel="stylesheet" href="ecommerce.css">
+    <link rel="stylesheet" href="index.css">
+    <title>TecJaguar</title>
+</head>
+<body>
+    <?php 
+        include_once("header.html");
+    ?>
+
+    <p style="top: 50%; color: white;">teste</p>
+    <p style="top: 50%; color: white;">teste</p>
+    <p style="top: 50%; color: white;">teste</p>
+    <p style="top: 50%; color: white;">teste</p>
+    <p style="top: 50%; color: white;">teste</p>
+    <p style="top: 50%; color: white;">teste</p>
+    <p style="top: 50%; color: white;">teste</p>
+    <p style="top: 50%; color: white;">teste</p>
+    <p style="top: 50%; color: white;">teste</p>
+    <p style="top: 50%; color: white;">teste</p>
+    <p style="top: 50%; color: white;">teste</p>
+    <p style="top: 50%; color: white;">teste</p>
+    <p style="top: 50%; color: white;">teste</p>
+    <p style="top: 50%; color: white;">teste</p>
+    <p style="top: 50%; color: white;">teste</p>
+    <p style="top: 50%; color: white;">teste</p>
+    <p style="top: 50%; color: white;">teste</p>
+    <p style="top: 50%; color: white;">teste</p>
+    <p style="top: 50%; color: white;">teste</p>
+    <p style="top: 50%; color: white;">teste</p>
+    <p style="top: 50%; color: white;">teste</p>
+    <p style="top: 50%; color: white;">teste</p>
+    <p style="top: 50%; color: white;">teste</p>
+    <p style="top: 50%; color: white;">teste</p>
+    <p style="top: 50%; color: white;">teste</p>
+    <p style="top: 50%; color: white;">teste</p>
+    <p style="top: 50%; color: white;">teste</p>
+    <p style="top: 50%; color: white;">teste</p>
+    <p style="top: 50%; color: white;">teste</p>
+    <p style="top: 50%; color: white;">teste</p>
+    <p style="top: 50%; color: white;">teste</p>
+    <p style="top: 50%; color: white;">teste</p>
+    <p style="top: 50%; color: white;">teste</p>
+    <p style="top: 50%; color: white;">teste</p>
+    <p style="top: 50%; color: white;">teste</p>
+    <p style="top: 50%; color: white;">teste</p>
+    <p style="top: 50%; color: white;">teste</p>
+    <p style="top: 50%; color: white;">teste</p>
+    <p style="top: 50%; color: white;">teste</p>
+
+    <?php 
+        include_once("footer.html");
+    ?>
+</body>
+</html>
