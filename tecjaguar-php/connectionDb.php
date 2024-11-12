6537b702ab4cab2684c1d186fee12d67ab34ac9a
Patrick Henrique da Silva Santos <106818806+Parriph13@users.noreply.github.com>
2024-11-11 22:37:48 -0300

    add for patrick

---
+++ tecjaguar-php/connectionDb.php
@@ -0,0 +1,17 @@
+<?php
+    //Variáveis de conexão ao banco de dados
+    $dbHost = 'estoquepro-estacio.c1eiug8i6wfp.us-east-1.rds.amazonaws.com';
+    $dbUser = 'admin';
+    $dbPassword = '6EQAMjuwKHShMotG0MAv';
+    $dbName = 'estoque';
+
+    //Variável de conexão
+    $connectionDb = new mysqli($dbHost, $dbUser, $dbPassword, $dbName);
+
+    //Estrutura condicional
+    if($connectionDb->connect_errno) {
+        echo "FALHA AO CONECTAR COM O BANCO DE DADOS";
+    } else {
+        echo "BANCO DE DADO CONECTADO COM SUCESSO";
+    }
+?>
